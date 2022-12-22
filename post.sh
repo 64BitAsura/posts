ff5cc52fb13b2ad9dcc7ca419718be9b3a4a95c5
Sambath Kumar Logakrishnan <6957824+64BitAsura@users.noreply.github.com>
2022-12-22 11:58:33 +0100

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,11 +1,11 @@
 upload_init_body='{ "registerUploadRequest": { "recipes": [ "urn:li:digitalmediaRecipe:feedshare-image" ], "owner": "urn:li:person:jzEm-jsKUu", "serviceRelationships": [ { "relationshipType": "OWNER", "identifier": "urn:li:userGeneratedContent" } ] } }'
 
 
-git show --name-only --oneline |
+git show --name-only --oneline main |
   rg 'post-[0-9]*/' -o $1 |
   uniq | xargs echo
 
-git show --name-only --oneline |
+git show --name-only --oneline main |
   rg 'post-[0-9]*/' -o $1 |
   uniq |
   while read file;
